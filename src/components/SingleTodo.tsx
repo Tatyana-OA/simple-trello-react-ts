fcf1b28de9680a96314cb73e05a9e979e11caa1f
Tatyana Asenova <tasenova@devrix.com>
2022-02-22 18:05:10 +0200

    Delete functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -14,6 +14,9 @@ const SingleTodo = ({todo, todos, setTodos}: Props) => {
 	const handleDone = (id:number) => {
 		setTodos(todos.map((todo) => todo.id===id?{...todo, isDone: !todo.isDone} : todo))
 	}
+	const handleDelete = (id:number) => {
+		setTodos(todos.filter((todo) => todo.id !== id))
+	}
   return (
 	<form className="todos__single">
 		{todo.isDone ?(
@@ -24,7 +27,7 @@ const SingleTodo = ({todo, todos, setTodos}: Props) => {
 
 		<div>
 			<span className="icon"><AiFillEdit/></span>
-			<span className="icon"><AiFillDelete /></span>
+			<span className="icon" onClick={()=> handleDelete(todo.id)}><AiFillDelete /></span>
 			<span className="icon" onClick={()=> handleDone(todo.id)}><MdDone/></span>
 		</div>
 	</form>
